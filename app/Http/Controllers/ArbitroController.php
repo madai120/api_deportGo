6c7b77fa21559622aa79a94d0be5f7370625a78d
JaimeRax <114894998+JaimeRax@users.noreply.github.com>
2024-05-28 17:22:43 -0600

    :bug: (bugfix): add controller in header import routes (#20)

--- app/Http/Controllers/ArbitroController.php
+++ app/Http/Controllers/ArbitroController.php
@@ -12,40 +12,38 @@ class ArbitroController extends Controller
     //Listar Arbitro
     public function listarArbitro()
     {
-        try{
+        try {
             $arbitro = Arbitro::where('estado', 1)->get();
-        return response()->json($arbitro);
-        }
-        catch(\Throwable $th){
+            return response()->json($arbitro);
+        } catch (\Throwable $th) {
             return response()->json([
                 'message'=>$th->getMessage(),
                 'status'=>false
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-    
+
         return response()->json([
             'message'=> 'okey',
             'status'=>true,
             'arbitro' => $arbitro,
         ], Response::HTTP_OK);
-        
     }
     //crear Arbitro
-   public function crearArbitro(Request $request){
-    
-    try{
-        $validator = Validator::make($request->all(), [
+    public function crearArbitro(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
             'primer_nombre' => 'required|string|max:255',
             'primer_apellido' => 'required|string|max:255',
             'direccion' => 'required|string|max:255',
             'telefono' => 'required|numeric|min:8'
         ]);
-    
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
-        }
 
-        $arbitro = Arbitro::create([
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+            }
+
+            $arbitro = Arbitro::create([
             'primer_nombre' => $request->primer_nombre,
             'segundo_nombre' => $request->segundo_nombre,
             'primer_apellido'=> $request->primer_apellido,
@@ -54,23 +52,22 @@ class ArbitroController extends Controller
             'direccion'=> $request->direccion,
             'telefono' => $request->telefono,
         ]);
-    }
-    catch(\Throwable $th){
-        return response()->json([
+        } catch (\Throwable $th) {
+            return response()->json([
             'message'=>$th->getMessage(),
             'status'=>false
         ], Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
+        }
 
-    return response()->json([
+        return response()->json([
         'message'=> 'okey',
         'status'=>true,
         'arbitro' => $arbitro,
     ], Response::HTTP_OK);
-   }
+    }
 
-   //Consultar Arbitro
-   public function consultarArbitro($id)
+    //Consultar Arbitro
+    public function consultarArbitro($id)
     {
         try {
             $arbitro = Arbitro::find($id);
@@ -78,7 +75,7 @@ class ArbitroController extends Controller
                 return response()->json(['message' => 'Arbitro no encontrado'], 404);
             }
             return response()->json($arbitro, 200);
-        } catch(\Throwable $th) {
+        } catch (\Throwable $th) {
             return response()->json([
                 'message' => $th->getMessage(),
                 'status' => false
@@ -89,46 +86,44 @@ class ArbitroController extends Controller
     //Editar Arbitro
     public function editarArbitro(Request $request, $id)
     {
-        try{
+        try {
             $arbitro = Arbitro::find($id);
             if (!$arbitro) {
                 return response()->json(['message' => 'Arbitro no encontrado'], 404);
             }
-    
+
             $validator = Validator::make($request->all(), [
                 'primer_nombre' => 'required|string|max:255',
                 'primer_apellido' => 'required|string|max:255',
                 'direccion' => 'required|string|max:255',
                 'telefono' => 'required|numeric|min:8'
             ]);
-    
+
             if ($validator->fails()) {
                 return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
             }
-    
+
             $arbitro->update($request->all());
-    
+
             return response()->json([
                 'message' => 'Arbitro actualizado exitosamente',
                 'status' => true,
                 'arbitro' => $arbitro,
             ], Response::HTTP_OK);
-        }
-        catch(\Throwable $th){
+        } catch (\Throwable $th) {
             return response()->json([
                 'message'=>$th->getMessage(),
                 'status'=>false
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-    
+
         return response()->json([
             'message'=> 'okey',
             'status'=>true,
             'arbitro' => $arbitro,
         ], Response::HTTP_OK);
-        
     }
-    
+
     public function desactivarArbitro($id)
     {
         try {
@@ -144,8 +139,7 @@ class ArbitroController extends Controller
                 'status' => true,
                 'patrocinador' => $arbitro,
             ], Response::HTTP_OK);
-
-        } catch(\Throwable $th) {
+        } catch (\Throwable $th) {
             return response()->json([
                 'message' => $th->getMessage(),
                 'status' => false
--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -12,65 +12,84 @@ class CategoriaController extends Controller
     //Listar categoria
     public function listCat()
     {
-        $categoria = Categoria::all();
-        return response()->json($categoria);
+        try {
+            $categoria = Categoria::all();
+            return response()->json($categoria);
+        } catch (\Throwable $th) {
+            return response()->json([
+            'message' => $th->getMessage(),
+            'status' => false
+        ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
     //crear categoria
-   public function crearCat(Request $request){
-    
-    try{
-        $validator = Validator::make($request->all(), [
+    public function crearCat(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
             'categoria' => 'required|string|max:255',
         ]);
-    
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
-        }
 
-        $categoria = Categoria::create([
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+            }
+
+            $categoria = Categoria::create([
             'categoria' => $request->categoria
         ]);
-    }
-    catch(\Throwable $th){
-        return response()->json([
+        } catch (\Throwable $th) {
+            return response()->json([
             'message'=>$th->getMessage(),
             'status'=>false
         ], Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
+        }
 
-    return response()->json([
+        return response()->json([
         'message'=> 'okey',
         'status'=>true,
         'categoria' => $categoria,
     ], Response::HTTP_OK);
-   }
-   //Consultar Categoria
-   public function consultarCat($id)
-{
-    $categoria = Categoria::find($id);
-    if (!$categoria) {
-        return response()->json(['message' => 'Categoria no encontrado'], 404);
     }
-    return response()->json($categoria, 200);
-}
-//Editar categoria
+    //Consultar Categoria
+    public function consultarCat($id)
+    {
+        try {
+            $categoria = Categoria::find($id);
+            if (!$categoria) {
+                return response()->json(['message' => 'Categoria no encontrado'], 404);
+            }
+            return response()->json($categoria, 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+            'message' => $th->getMessage(),
+            'status' => false
+        ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+    //Editar categoria
     public function editarCat(Request $request, $id)
     {
-        $categoria = Categoria::find($id);
-        if (!$categoria) {
-            return response()->json(['message' => 'categoria no encontrado'], 404);
-        }
+        try {
+            $categoria = Categoria::find($id);
+            if (!$categoria) {
+                return response()->json(['message' => 'categoria no encontrado'], 404);
+            }
 
-        $validator = Validator::make($request->all(), [
+            $validator = Validator::make($request->all(), [
             'categoria' => 'required|string|max:255'
         ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+            }
+
+            $categoria->update($request->all());
+        } catch (\Throwable $th) {
+            return response()->json([
+            'message' => $th->getMessage(),
+            'status' => false
+        ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        $categoria->update($request->all());
-
         return response()->json([
             'message' => 'Categoria actualizado exitosamente',
             'status' => true,
